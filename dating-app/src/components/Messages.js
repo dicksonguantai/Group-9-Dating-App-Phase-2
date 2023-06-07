2950c4b034c8db6fc7135fee7a8c1a4f7756f8d4
Maureen Ndereba <117657621+maureen-ndereba@users.noreply.github.com>
2023-06-07 13:09:56 +0300

    Structure for Messages

--- dating-app/src/components/Messages.js
+++ dating-app/src/components/Messages.js
@@ -1,1 +1,38 @@
-//Add messages page code 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Messages() {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const response = await axios.get('API_ENDPOINT_URL');
+        setChats(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchChats();
+  }, []);
+
+  // Render user profile photos window
+  const userProfilePhotos = chats.map((chat) => (
+    <img key={chat.id} src={chat.userProfilePhoto} alt="User Profile" />
+  ));
+
+  // Render chat messages
+  const renderChatMessages = () => {
+    // Logic for rendering chat messages
+  };
+
+  return (
+    <div>
+      <div className="user-profile-photos-window">{userProfilePhotos}</div>
+      <div className="chat-window">{renderChatMessages()}</div>
+    </div>
+  );
+}
+
+export default Messages;
