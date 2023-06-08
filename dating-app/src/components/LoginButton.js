77367378fc7e3fa8033f19289a0b47b983ec3867
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-06-08 16:23:16 +0300

    corrections

--- dating-app/src/components/LoginButton.js
+++ dating-app/src/components/LoginButton.js
@@ -1,34 +1,21 @@
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// import React from 'react'
-
-// const LoginButton = () => {
-//     const { loginWithRedirect, isAuthenticated } = useAuth0();
-
-//   return (
-//       !isAuthenticated && (
-//         <button onClick={() => loginWithRedirect()}> 
-//             Sign In      
-//         </button>
-
-//       )
-    
-//   )
-// }
-
-// export default LoginButton
+import { useAuth0 } from "@auth0/auth0-react";
 
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
 
 const LoginButton = () => {
-    const { LoginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    <button onClick={() => LoginWithRedirect()}>
-        Log In      
-    </button>
+      !isAuthenticated && (
+        <button onClick={() => loginWithRedirect()}> 
+            Log In      
+        </button>
+
+      )
+    
   )
 }
 
 export default LoginButton
+
 
