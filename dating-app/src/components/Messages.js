3fe51ba9c39347ceb2044f45f8b11f4c0bcf1013
Maureen Ndereba <117657621+maureen-ndereba@users.noreply.github.com>
2023-06-12 17:52:12 +0300

    Messages UI improvements

--- dating-app/src/components/Messages.js
+++ dating-app/src/components/Messages.js
@@ -9,55 +9,78 @@ import userIcon from './user-icon.png';
 
 function Messages() {
 
-    const [chatData, setChatData] = useState([]); 
+    const [chatData, setChatData] = useState([]);
     const [selectedChat, setSelectedChat] = useState(null);
-    const [inputText, setInputText] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [messages, setMessages] = useState([sortByTime(data.users[0].messages)]);
+    const [newMessageInput, setNewMessageInput] = useState("");
     let chatMessages;
 
 
     //Set my data
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            setChatData(data.users);
-            if (data.users.length > 0) {
-              setSelectedChat(data.users[0]);
+            try {
+                setChatData(data.users);
+                if (data.users.length > 0) {
+                    setSelectedChat(data.users[0]);
+                }
+            } catch (error) {
+                console.error('Error fetching chat data:', error);
             }
-          } catch (error) {
-            console.error('Error fetching chat data:', error);
-          }
         };
-    
+
         fetchData();
-      }, []);
+    }, []);
 
 
     //My Events 
     //Load chat messages when chat profile is clicked. 
     const handleChatClick = (chat) => {
+        console.log("Selected chat");
+        console.log(chat);
+        let newMessages = sortByTime(chat.messages);
+
         setSelectedChat(chat);
+        setMessages([newMessages]);
         renderMessages();
     };
 
+    //Search 
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+      };
     //Send message click event 
-    const handleSendMessage = () => {
-        if (inputText.trim() !== '') {
-            const newMessage = {
-                //message: newMessageInput,
-                time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }),
-                type: 'sent',
-            };
-            
-            chatMessages.push(newMessage)
-            renderMessages();
-        }
+    const handleInputChange = (event) => {
+        setNewMessageInput(event.target.value);
+    };
 
+    const handleSendMessage = () => {
+        const newMessage = {
+            message: newMessageInput,
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }),
+            type: 'sent',
+        };
+
+        setMessages(prevMessages => {
+            const updatedMessages = [...prevMessages];
+            if (updatedMessages.length > 0) {
+                if (!updatedMessages[0].messages) {
+                    updatedMessages[0].messages = []; // Create messages array if it doesn't exist
+                }
+                updatedMessages[0] = [...updatedMessages[0], newMessage];
+            }
+            return updatedMessages;
+        });
+
+        setNewMessageInput("");
     };
 
     //Other functions
     //Function to sort my messages by time
     function sortByTime(array) {
-        return array.sort((a, b) => {
+
+        let newarray = array.sort((a, b) => {
             const [aHour, aMinute, aPeriod] = a.time.split(/:|\s/);
             const [bHour, bMinute, bPeriod] = b.time.split(/:|\s/);
 
@@ -78,12 +101,17 @@ function Messages() {
 
             return 0;
         });
+        return newarray;
     }
 
 
     //Function to set contact list
     const renderMemberCards = () => {
-        return chatData.map((chat, index) => (
+        const filteredMembers = chatData.filter((chat) =>
+      chat.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+        return filteredMembers.map((chat, index) => (
             <React.Fragment key={chat.id}>
                 <div
                     className={`member-card ${selectedChat && selectedChat.id === chat.id ? 'active' : ''}`}
@@ -105,12 +133,13 @@ function Messages() {
         if (!selectedChat || !selectedChat.messages) {
             return null;
         }
-        const messages = sortByTime(selectedChat.messages);
-
+        //const messages = sortByTime(selectedChat.messages);
+        console.log("Display Message: ");
+        console.log(messages);
         return (
             <div className='chatbox-container'>
                 <ul className="message-list">
-                    {messages.map((message, index) => (
+                    {messages[0].map((message, index) => (
                         <li
                             key={index}
                             className={`message-item ${message.type === 'sent' ? 'sent' : 'received'}`}
@@ -136,7 +165,12 @@ function Messages() {
                         <h2 className="title">Conversations</h2>
                     </div>
                     <div className="input-bar">
-                        <input type="text" placeholder="Search profile" />
+                        <input
+                            type="text"
+                            placeholder="Search profile"
+                            value={searchQuery}
+                            onChange={handleSearchInputChange}
+                        />
                     </div>
                 </div>
 
@@ -160,7 +194,11 @@ function Messages() {
                     </div>
                 </div>
                 <div className="bottom-section">
-                    <input type="text" placeholder="Type your message" className="input-field" /*value={newMessageInput}*//>
+                    <input type="text" placeholder="Type your message" className="input-field" value={newMessageInput} onChange={handleInputChange} onKeyPress={(event) => {
+                        if (event.key === "Enter") {
+                            handleSendMessage();
+                        }
+                    }} />
                     <button className="send-button" onClick={handleSendMessage}>Send</button>
                 </div>
             </div>
